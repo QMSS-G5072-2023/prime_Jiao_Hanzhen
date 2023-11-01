72bfe87314101a6e61bc7e9adb14791d3223b107
Jiao_Hanzhen <hj2667@columbia.edu>
2023-11-01 15:18:27 -0400

    Test prime

--- tests/test_prime_hj2667.py
+++ tests/test_prime_hj2667.py
@@ -1,1 +1,10 @@
-from prime_hj2667 import prime_hj2667
+from prime_hj2667 import is_prime
+
+def test_is_prime():
+    assert is_prime(-1) == False
+    assert is_prime(0) == False
+    assert is_prime(1) == False
+    assert is_prime(2) == True
+    assert is_prime(3) == True
+    assert is_prime(5) == True
+    assert is_prime(13) == True
